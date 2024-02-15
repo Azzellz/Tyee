ce43c6a3f9453f707554df9ed0af7bc252c5c622
Lin <2295141009@stu.hqu.edu.cn>
2024-02-15 11:59:46 +0800

    适配移动端

--- src/views/Home-View.vue
+++ src/views/Home-View.vue
@@ -2,30 +2,22 @@
     <div id="container">
         <n-space vertical>
             <n-gradient-text type="info">梦是现实的延续。 </n-gradient-text>
-            <n-gradient-text type="danger">现实是梦的终结； </n-gradient-text>
+            <n-gradient-text type="danger">现实是梦的终结；</n-gradient-text>
         </n-space>
     </div>
 </template>
 
 <script setup lang="ts">
-import { NGradientText, NSpace } from 'naive-ui'
-import { ref, onMounted, onUnmounted } from 'vue'
-
-//定时切换标题
+import { NSpace, NGradientText } from 'naive-ui'
 </script>
 
 <style lang="less" scoped>
 #container {
-    width: 100%;
+    width: 400px;
     height: 100%;
     /* 确保伪元素不会超出父元素的边界 */
     position: relative;
     overflow: hidden;
-    padding: 50px;
-    // display: flex;
-    // flex-direction: column;
-    // justify-content: space-around;
-    // align-items: center;
     ::before {
         content: '';
         position: absolute;
@@ -33,7 +25,7 @@ import { ref, onMounted, onUnmounted } from 'vue'
         right: 0;
         bottom: 0;
         left: 0;
-        // background-image: url('../assets/img/bg/hoshino.png');
+        background-image: url('../assets/img/bg/hoshino.png');
         background-size: cover;
         background-repeat: no-repeat;
         filter: blur(5px); /* 设置模糊效果 */
